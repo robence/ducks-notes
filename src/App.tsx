a3691a293d129e4e76a2277b5e62bb3a76804bc5
rozsabence <robence@gmail.com>
2019-06-10 19:31:20 +0200

    add dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/layout/Header';
+import Dashboard from './components/layout/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Dashboard></Dashboard>
     </div>
   );
 }
